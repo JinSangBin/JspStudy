ee9fc715b6ff3ecc20284d3a380bbe8bc60bbda9
DESKTOP-EOKTSLO\kosmo <wlswlswls369@naver.com>
2023-04-25 18:07:19 +0900

    0425

---
+++ 0425/P07UseForm-2/src/main/java/com/study/springboot/P07UseForm2Application.java
@@ -0,0 +1,13 @@
+package com.study.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P07UseForm2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P07UseForm2Application.class, args);
+	}
+
+}
---
+++ 0425/P09UseValidator/src/main/java/com/study/springboot/ContentDTO.java
@@ -0,0 +1,12 @@
+package com.study.springboot;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ContentDTO {
+	private int id;
+	private String writer;
+	private String content;
+}
